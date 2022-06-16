87f942ff1e6273cd08d34cfb4b04621c1eefe83a
steven <stevenbaezfernandez@gmail.com>
2022-06-16 13:26:54 -0400

    updating database connection inf

--- Backend/db.php
+++ Backend/db.php
@@ -3,10 +3,10 @@
     class Conexion extends mysqli{
 
         // atributos de clase
-        private $host = "127.0.0.1";
-        private $user = "root";
-        private $pass = "";
-        private $db_name = "loginsystem";
+        private $host = "us-cdbr-east-05.cleardb.net";
+        private $user = "be6f19ebd6435e";
+        private $pass = "5f468a0a";
+        private $db_name = "heroku_af4ced14dbc508a";
 
         function __construct(){
             parent:: __construct($this -> host, $this -> user, $this -> pass, $this -> db_name, );
